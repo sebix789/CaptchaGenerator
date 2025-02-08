2fd23c41f8d98ae13dbf3f3c329c79c205c556d0
Szymon Brud <szymon.brud@crodu.com>
2025-02-08 23:59:22 +0100

    Configure Tailwind CSS

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { GlobalStyle } from './indexStyle';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import './index.css'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
